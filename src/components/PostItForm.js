2db123da6aedc8e690cb78e7b3b6cdcaf902ac64
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-03-05 20:33:52 +0000

    failing tests in __postitform - it doesnt like the refs getDOMNode parts

--- src/components/PostItForm.js
+++ src/components/PostItForm.js
@@ -1,11 +1,16 @@
 var React = require("react");
 
 var PostItForm = React.createClass({
+	eHandler: function(e) {
+		e.preventDefault();
+		this.refs.textfield.getDOMNode().textContent = "";
+		console.log(this.refs.textfield.getDOMNode().value);
+	},
     render: function() {
         return (
-            <form>
-                <input type="text" className="postItText" />
-                <input type="submit" className="postItSubmit" />
+            <form className="col-md-3" ref="commentform" onSubmit={this.eHandler}>
+                <input type="text" className="postItText" ref="textfield" />
+                <input type="submit" className="postItSubmit" ref="textsubmit" />
             </form>
         );
     }
